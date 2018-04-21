c7437d8660b5bf7a1cb04e7e3884b167c682eb19
John Powers <john_powers2@cable.comcast.com>
2018-04-21 11:51:06 -0400

    now streams data and decrypts in chunks
    cleanup
    
    configurable chunk size
    
    configurable chunk size
    
    clean up

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ As per https://www.twilio.com/docs/voice/tutorials/call-recording-encryption
 `pip install -r requirements.txt`
 
 ## Configure
-Replace `TWILIO_SID`, `TWILIO_AUTH_TOKEN`, `PRIVATE_KEY`, `PORT` in config.py as needed
+Replace `TWILIO_SID`, `TWILIO_AUTH_TOKEN`, `PRIVATE_KEY`, `PORT`, `CHUNK_SIZE` in config.py as needed
 
 ## Run server
 `python twilio_decryption.py`
 
 ## Listen to decrypted recording
-Point browser to http://localhost:4200/<RECORDING_SID> 
+Point browser to http://localhost:4200/<RECORDING_SID>
--- twilio_decryption.py
+++ twilio_decryption.py
@@ -1,27 +1,22 @@
 import requests
-from flask import Flask, Response
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+from config import TWILIO_SID, TWILIO_AUTH_TOKEN, PORT, PRIVATE_KEY, CHUNK_SIZE
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from flask import Flask, Response
 from requests.auth import HTTPBasicAuth
 from twilio.rest import Client
 
-import config
-
 app = Flask(__name__)
-
-TWILIO_SID = config.TWILIO_SID
-TWILIO_AUTH_TOKEN = config.TWILIO_AUTH_TOKEN
-PORT = config.PORT
-PRIVATE_KEY = config.PRIVATE_KEY
 
 @app.route('/<recording_sid>')
 def decrypt_recording(recording_sid):
   client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
   encrypted_recording_url = "https://api.twilio.com/2010-04-01/Accounts/%s/Recordings/%s" %(TWILIO_SID, recording_sid)
 
-  encrypted_audio = requests.get(encrypted_recording_url)
+  encrypted_audio = requests.get(encrypted_recording_url, stream=True)
 
   # Following "Per Recording Decryption Steps (Customer)"
   # https://www.twilio.com/docs/voice/tutorials/call-recording-encryption#per-recording-decryption-steps-customer
@@ -31,20 +26,25 @@ def decrypt_recording(recording_sid):
   # by performing a GET on the recording resource
   recording = client.recordings(sid=recording_sid).fetch()
   encryption_details = recording.encryption_details
-  encrypted_cek = encryption_details.get('encrypted_cek').decode('base64')
-  iv = encryption_details.get('iv').decode('base64')
+  encrypted_cek = encryption_details.get('encrypted_cek')
+  iv = encryption_details.get('iv')
   public_key_sid = encryption_details.get('public_key_sid')
 
   # 2) Retrieve customer private key corresponding to public_key_sid and
   # use it to decrypt base 64 decoded encrypted_cek via RSAES-OAEP-SHA256-MGF1
   key = RSA.importKey(PRIVATE_KEY)
   rsa_cipher = PKCS1_OAEP.new(key, hashAlgo=SHA256)
-  decrypted_cek = rsa_cipher.decrypt(encrypted_cek)
+  decrypted_cek = rsa_cipher.decrypt(encrypted_cek.decode('base64'))
 
-  # 3) Initialize a AES256-GCM SecretKey object with decrypted CEK (and base 64 decoded iv)
-  decryptor = AESGCM(decrypted_cek)
-  # 4) Decrypt encrypted recording using the SecretKey (and base 64 decoded iv)
-  decrypted_audio = decryptor.decrypt(iv, encrypted_audio.content, None)
+  # 3) Initialize a AES256-GCM SecretKey object with decrypted CEK and base 64 decoded iv
+  decryptor = Cipher(
+    algorithms.AES(decrypted_cek),
+    modes.GCM(iv.decode('base64')),
+    backend=default_backend()
+  ).decryptor()
+
+  # 4) Decrypt encrypted recording using the SecretKey
+  decrypted_audio = (decryptor.update(chunk) for chunk in encrypted_audio.iter_content(CHUNK_SIZE))
 
   return Response(decrypted_audio, content_type=encrypted_audio.headers['content-type'])
 
